85ad85c96e061183949965fb94c561ac372c6e45
Dmitry <mityasazonow@yandex.ru>
2018-08-28 16:08:41 +0300

    NIPT

--- AD-VI/js/common.js
+++ AD-VI/js/common.js
@@ -29,19 +29,21 @@ $(window).scroll(function () {
     console.log(dy);
     console.log(wh);
 
-    if (dy < wh / 5) {
+    if (dy < wh / 4) {
 
         $('#tabletka').css('transform', 'rotate(0deg)');
         $('#tabletka').animate({
                 'left': -dy,
                 'top': 0
-            }, {
+            },{
                 duration: 0,
                 easing: 'linear'
             }
         );
 
     } else if (dy < 2 * wh - 400) {
+
+
 
         $('#tabletka').css('transform', 'rotate(-90deg)');
         $('#tabletka').animate({
@@ -52,6 +54,16 @@ $(window).scroll(function () {
             }
         );
 
+
+        $('#tabletka span').css({
+            'display': 'none'
+        });
+        $('#razrab, #marketing, #branding, #analiz').animate({
+            'opacity':0,
+            'top':0,
+            'left':'0'
+        },300);
+
         $('#tabletka>.yel').css({
             'transform': 'rotate(0deg)'
         });
@@ -59,13 +71,43 @@ $(window).scroll(function () {
             'transform': 'rotate(0deg)'
         });
     } else {
-
-        $('#tabletka').css('transform', 'rotate(0deg)');
+        var tl = $('#tabletka').css('left');
+        $('#tabletka').css({
+            'transform': 'rotate(0deg)'
+        });
         $('#tabletka>.yel').css({
             'transform': 'rotate(45deg)'
         });
         $('#tabletka>.blue').css({
             'transform': 'rotate(-15deg)'
         });
+        $('#tabletka span').css({
+            'display': 'block'
+        });
+
+
+        $('#razrab').animate({
+            'opacity':1,
+            'top':600,
+            'left':50
+        },0);
+
+        $('#marketing').animate({
+            'opacity':1,
+            'top':425,
+            'left':390
+        },0);
+
+        $('#branding').animate({
+            'opacity':1,
+            'top':400,
+            'left':35
+        },0);
+
+        $('#analiz').animate({
+            'opacity':1,
+            'top':250,
+            'left':80
+        },0);
     }
 });
