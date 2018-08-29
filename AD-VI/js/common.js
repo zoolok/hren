7a8761f1141bf149cab2ee8099bc257f93eb34d4
Dmitry <mityasazonow@yandex.ru>
2018-08-29 13:18:56 +0300

    NIPT

--- AD-VI/js/common.js
+++ AD-VI/js/common.js
@@ -1,11 +1,11 @@
 /* --------------------------------------------
- MAIN FUNCTION
+MAIN FUNCTION
 -------------------------------------------- */
 $(document).ready(function () {
 
     /* --------------------------------------------------------
- ANIMATED SCROLL PAGE WITH ACTIVE MENU - BOOTSTRAP SROLLSPY
------------------------------------------------------------ */
+    ANIMATED SCROLL PAGE
+    ----------------------------------------------------------- */
     $(function () {
         "use strict";
         $(".mouse-icon").on('click', function (event) {
@@ -14,14 +14,14 @@ $(document).ready(function () {
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 1000, function () {
-                window.location.hash = hash;
-            });
+            }, 1000);
         });
     });
 });
 
-
+/* --------------------------------------------------------
+TABLETKA DRIVE
+----------------------------------------------------------- */
 $(window).scroll(function () {
     var dy = $(this).scrollTop();
     var wh = window.innerHeight;
@@ -29,20 +29,19 @@ $(window).scroll(function () {
     console.log(dy);
     console.log(wh);
 
-    if (dy < wh / 4) {
+    if (dy < wh / 5) {
 
         $('#tabletka').css('transform', 'rotate(0deg)');
         $('#tabletka').animate({
                 'left': -dy,
                 'top': 0
-            },{
+            }, {
                 duration: 0,
                 easing: 'linear'
             }
         );
 
     } else if (dy < 2 * wh - 400) {
-
 
 
         $('#tabletka').css('transform', 'rotate(-90deg)');
@@ -54,15 +53,11 @@ $(window).scroll(function () {
             }
         );
 
-
-        $('#tabletka span').css({
-            'display': 'none'
+        $('#razrab, #marketing, #branding, #analiz').css({
+            'opacity': 0,
+            'top': 0,
+            'left': 0,
         });
-        $('#razrab, #marketing, #branding, #analiz').animate({
-            'opacity':0,
-            'top':0,
-            'left':'0'
-        },300);
 
         $('#tabletka>.yel').css({
             'transform': 'rotate(0deg)'
@@ -70,8 +65,10 @@ $(window).scroll(function () {
         $('#tabletka>.blue').css({
             'transform': 'rotate(0deg)'
         });
+
+
     } else {
-        var tl = $('#tabletka').css('left');
+
         $('#tabletka').css({
             'transform': 'rotate(0deg)'
         });
@@ -81,33 +78,72 @@ $(window).scroll(function () {
         $('#tabletka>.blue').css({
             'transform': 'rotate(-15deg)'
         });
-        $('#tabletka span').css({
-            'display': 'block'
-        });
 
-
-        $('#razrab').animate({
-            'opacity':1,
-            'top':600,
-            'left':50
-        },0);
+        $('#tabletka span#razrab').animate({
+            'opacity': 1,
+            'top': 600,
+            'left': 50
+        }, 10);
 
         $('#marketing').animate({
-            'opacity':1,
-            'top':425,
-            'left':390
-        },0);
+            'opacity': 1,
+            'top': 425,
+            'left': 390
+        }, 10);
+
 
         $('#branding').animate({
-            'opacity':1,
-            'top':400,
-            'left':35
-        },0);
+            'opacity': 1,
+            'top': 400,
+            'left': 35
+        }, 10);
+
 
         $('#analiz').animate({
-            'opacity':1,
-            'top':250,
-            'left':80
-        },0);
+            'opacity': 1,
+            'top': 250,
+            'left': 80
+        }, 10);
+
     }
+});
+
+/* --------------------------------------------------------
+SERVICES POPUP
+----------------------------------------------------------- */
+$('#tabletka span').click(function (e) {
+    e.preventDefault();
+
+    var bn = $(this).attr('id');
+
+    $('.serv-pop-up').each(function (index) {
+
+        if ($(this).css('opacity') == 1) {
+            $(this).find('.close-pop-up').trigger('click');
+        }
+
+        if ($(this).attr('data') == bn) {
+
+            $(this).animate({
+                'width': 1000,
+                'height': 1000,
+                'opacity': 1
+            }).css({
+                'top': 'calc(50% - 30rem)',
+                'left': 'calc(50% - 30rem)'
+            });
+        }
+    });
+
+});
+$('.close-pop-up').click(function (e) {
+    e.preventDefault();
+    $(this).parent().animate({
+        'width': 0,
+        'height': 0,
+        'opacity': 0
+    }).css({
+        'top': 0,
+        'left': 0
+    });
 });
